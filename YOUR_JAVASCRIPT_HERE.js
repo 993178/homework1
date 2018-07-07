219158980a48e5d9d17d5ed254efb238c78766af
993178 <papilio29@hotmail.com>
2018-07-07 14:54:20 +0200

    zucht. nee wacht. er zijn geloof ik wat fncties bijgekomen sinds de vorige commit

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -1,19 +1,25 @@
 // Write your JS here  Meh, I'll take the next line
 const hero = {
     name: "DI Goodman",
-    isChaotic: true,
-    //am I misunderstanding this boolean-property thing? Test won't OK this
+    heroic: true,
     inventory: [],
     health: 10,
     weapon: {
         type: "cause confusion",
         damage: 2
-    },
-    rest: function() {
-        health = health+1
-    },
-    pickUpItem: function(item) {
-        this.inventory.push(item)
-    }
+    }    
+}
+
+function rest(newHealth) {
+    hero.health = newHealth
+    return hero
+    console.log(hero.health);
+}
+function pickUpItem(item) {
+    hero.inventory.push(item)
+}
+function equipWeapon() {
 
 }
+
+document.getElementById("inn").onclick = rest(11)
